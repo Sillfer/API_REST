a7f71adcf251c4579e753fd9fb1164b151df0e5f
Elmahdi <gliouinemahdi0@gmail.com>
2022-03-10 09:13:08 +0100

    Create added

--- api/post/create.php
+++ api/post/create.php
@@ -1,18 +1,18 @@
 <?php
-//Header
+// Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers : Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
 include_once '../../models/Post.php';
 
-//Instaantiate DB & connect
+// Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
-//Instaantiate blog post object
+// Instantiate blog post object
 $post = new Post($db);
 
 // Get raw posted data
@@ -24,12 +24,12 @@ $post->author = $data->author;
 $post->category_id = $data->category_id;
 
 // Create post
-if($post->create()) {
-    echo json_encode(
-        array('message' => 'Post created')
-    );
+if ($post->create()) {
+  echo json_encode(
+    array('message' => 'Post Created')
+  );
 } else {
-    echo json_encode(
-        array('message' => 'Post not created')
-    );  
+  echo json_encode(
+    array('message' => 'Post Not Created')
+  );
 }
